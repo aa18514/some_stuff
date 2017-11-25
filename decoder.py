84d7b98921d58cb54af011071858cd6a96d8e13e
aa18514 <aa18514@ic.ac.uk>
2017-11-25 00:40:58 +0000

    lenet working

--- decoder.py
+++ decoder.py
@@ -41,18 +41,21 @@ def tf_conv_cpu(matrix_1, conv, pool, data, conv_1_weights, conv_2_weights, conv
     B = tf.nn.max_pool(A, [1, 2, 2, 1], [1, 2, 2, 1],  padding = "SAME") + conv1_bias
     C = tf.nn.conv2d(B, filter = conv_2_weights, strides = [1, 1, 1, 1],  padding = "VALID")
     relu = tf.nn.max_pool(C, [1, 2, 2, 1], [1, 2, 2, 1], padding = "SAME")
-    a = sess.run(relu)
-    b = a.transpose().transpose((3, 0, 2, 1)) + conv2_bias.reshape(1, 50, 1, 1)
-    Z = tf.matmul(ip1_weights.reshape(500, 800), b.reshape(10000, 800).T)
+    b = relu + conv2_bias.reshape(1, 1, 1, 50)
+    b = tf.reshape(b, (10000, 800))
+    b = tf.transpose(b)
+    Z = tf.matmul(ip1_weights.reshape(500, 800), b)
     Y = tf.matmul(ip2_weights.reshape(10, 500), tf.nn.relu(Z)) + ip2_bias.reshape(10, 1)
-    relu = sess.run(tf.nn.softmax(Y, dim = 0))
+    relu = tf.nn.softmax(Y, dim = 0)
+    relu = tf.argmax(relu, 0)
+    relu = sess.run(relu)
     sess.close()
     FinalTime = datetime.datetime.now()
-    relu[relu < 0.5] = 0
-    relu[relu >= 0.5] = 1
-    relu = relu.T
+    b = ((FinalTime - StartTime).total_seconds())
     print("tensorflow took: %fs" % (10000 * ((40 * 24 * 24 * 5 * 5) + (24 * 24 * 20) + (20 * 12 * 12 * 4) +  (2 * 12 * 12 * 5 * 5 * 20 * 50) + (50 * 20 * 12 * 12) + (50 * 12 * 12) + (50 * 8 * 8 * 4) +  (500 * 4 * 800) + (10 * 500 * 2))/(FinalTime - StartTime).total_seconds()))
     print((FinalTime - StartTime).total_seconds())
+    ImagesPerSecond = int(10000/((FinalTime - StartTime).total_seconds()))
+    print("images per second: %d" % ImagesPerSecond)
     return relu
 
 def numpy_conv(net, matrix_1): 
@@ -111,17 +114,17 @@ if __name__ == "__main__":
 	conv2_bias = np.array(hf.get('conv2_biases'))
 	hf.close()
 	hf = h5py.File('fc.h5', 'r')
-	fc1_weights = np.array(hf.get('fc1_weights'))
+	fc1_weights = np.array(hf.get('fc1_weights')).reshape(500, 50, 4, 4)  
+	fc1_weights = fc1_weights.transpose((0, 2, 3, 1))
 	fc1_bias = np.array(hf.get('fc1_biases'))
-	fc2_weights = np.array(hf.get('fc2_weights'))
+	fc2_weights = np.array(hf.get('fc2_weights')) 
 	fc2_bias = np.array(hf.get('fc2_biases'))
 	with open("t10k-images-idx3-ubyte", 'rb') as f: 
 		magic, num, rows, cols = struct.unpack(">IIII", f.read(16))
-		image = np.fromfile(f, dtype = np.uint8).reshape(num, rows, cols);
+		image = np.fromfile(f, dtype = np.uint8).reshape(num, rows, cols)
 	with open("t10k-labels-idx1-ubyte", 'rb') as f: 
 		magic_nr, size = struct.unpack(">II", f.read(8))
 		lbl = np.fromfile(f, dtype=np.int8)
-
-	print(len(lbl))
-	predicted_labels = tf_conv_cpu(np.array(image, dtype = np.float32), conv, pool, data, conv1_weights.reshape(5, 5, 1, 20), conv2_weights.reshape(5, 5, 20, 50), conv1_bias, conv2_bias, fc1_weights, fc2_weights, fc1_bias, fc2_bias)
-	print(np.sum(np.nonzero(predicted_labels)[1] != lbl)/100)
+	image = np.array(image, dtype = np.float32)/256
+	predicted_labels = tf_conv_cpu(image, conv, pool, data, conv1_weights.reshape(5, 5, 1, 20), conv2_weights.reshape(5, 5, 20, 50), conv1_bias, conv2_bias, np.array(fc1_weights, np.float32), np.array(fc2_weights, np.float32), fc1_bias, fc2_bias)
+	print("total error percentage is: %f" % (float(np.sum(predicted_labels != lbl))/100))
